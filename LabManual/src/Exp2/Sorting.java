e9ba4c27486c4324fcbcce9997b1b8e65faa02af
anand-krishanu <anandkrishanu26@gmail.com>
2025-02-19 13:55:08 +0530

    Exp 2

--- LabManual/src/Exp2/Sorting.java
+++ LabManual/src/Exp2/Sorting.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 
 public class Sorting {
     public static void main(String[] args) {
-        int[] arr = {98, 23, 42, 34, 76, 13, 17};
+        int[] arr = new int[]{98, 23, 42, 34, 76, 13, 17};
 
+        System.out.println(Arrays.toString(selectionSort(arr)));
+
+        arr = new int[]{98, 23, 42, 34, 76, 13, 17};
         System.out.println(Arrays.toString(insertionSort(arr)));
+
     }
 
     //Time Complexity of Insertion Sort: O(n^2)
@@ -23,5 +27,30 @@ public class Sorting {
             }
         }
         return arr;
+    }
+
+    //Time Complexity of Selection Sort: O(n^2)
+    //Space Complexity of Selection Sort: O(1)
+    public static int[] selectionSort (int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+
+            for (int j = i; j <arr.length; j++) {
+                if(arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+
+            if(i != minIndex) {
+                swap(arr, i, minIndex);
+            }
+        }
+        return arr;
+    }
+
+    public static void swap(int[] arr, int i, int minIndex) {
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 }
